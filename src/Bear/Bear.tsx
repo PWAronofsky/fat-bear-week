5f3a51cec9ddbd3302a85079924bbf6943974c9b
pwaronofsky <paularonofsky@gmail.com>
2025-06-25 16:02:39 -0400

    updated bear style

--- src/Bear/Bear.tsx
+++ src/Bear/Bear.tsx
@@ -14,15 +14,15 @@ export interface BearProps {
 
 export const Bear = ({ bear, pickThisBear, nodeId, offsetTop, offsetBottom }: BearProps) => {
   const [showAfterPic, setShowAfterPic] = React.useState(true);
-  // const [imgStyle, setImgStyle] = React.useState("missing-image");
+  const [imgStyle, setImgStyle] = React.useState("missing-image");
   const { canEditBracket } = useUserContext();
 
   const inputRef = React.useRef<HTMLInputElement>(null);
 
 
-  // React.useEffect(() => {
-  //   setImgStyle((showAfterPic && bear?.afterImgSrc) || bear?.beforeImgSrc ? "bear-image" : "missing-image");
-  // }, [showAfterPic, bear]);
+  React.useEffect(() => {
+    setImgStyle((showAfterPic && bear?.afterImgSrc) || bear?.beforeImgSrc ? "bear-image" : "missing-image");
+  }, [showAfterPic, bear]);
   const afterImage = bear?.afterImgSrc ? require(`../images/${bear?.afterImgSrc}`) : require('../images/confusedBear.png');
   const beforeImage = bear?.beforeImgSrc ? require(`../images/${bear?.beforeImgSrc}`) : require('../images/confusedBear.png');
 
@@ -33,8 +33,8 @@ export const Bear = ({ bear, pickThisBear, nodeId, offsetTop, offsetBottom }: Be
     <div className="bear column center" style={{ marginTop: offsetTop, marginBottom: offsetBottom }}>
       <input id={nodeId} ref={inputRef} type="image" 
         onClick={() => setShowAfterPic(!showAfterPic)} 
-        className={`bear-image shadowed`}
-        src={showAfterPic ? afterImage :beforeImage} 
+        className={`${imgStyle} shadowed`}
+        src={showAfterPic ? afterImage : beforeImage} 
         alt={a11yLabels.beforeAfterButton(showAfterPic, bear?.tagNumber, bear?.name)}/>
       { bear && (canEditBracket ? (
           <button aria-label={a11yLabels.pickBear(bear.tagNumber, bear?.name)} className="btn btn-secondary btn-sm shadowed" onClick={()=> pickThisBear(bear.id)}>
