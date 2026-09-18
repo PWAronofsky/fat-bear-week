b8c907643d2e024b8c25540b1ebe7de6ff1135a5
pwaronofsky <paularonofsky@gmail.com>
2026-09-18 19:36:22 -0400

    added bears to first round

--- src/mockData.ts
+++ src/mockData.ts
@@ -84,119 +84,186 @@ export const mockBears: BearType[] = [
     name: 'Holly',
     beforeImgSrc: '435-before.png',
     afterImgSrc: '435-after.png'
+  },
+  // TODO: placeholders for the 4 bears added to the field this year.
+  // Swap in real tag numbers, names, and before/after images.
+  {
+    id: 13,
+    tagNumber: 0,
+    name: 'TBD 1'
+  },
+  {
+    id: 14,
+    tagNumber: 0,
+    name: 'TBD 2'
+  },
+  {
+    id: 15,
+    tagNumber: 0,
+    name: 'TBD 3'
+  },
+  {
+    id: 16,
+    tagNumber: 0,
+    name: 'TBD 4'
   }
 ];
 
 export const mockMatchups: MatchupType[] = [
+  // Round 1 - left side
   {
     id: 1,
     bear1: mockBears[0],
     bear2: mockBears[1],
-    nextMatchup: 5,
+    nextMatchup: 9,
     pickedWinner: undefined,
     column: 1,
     bear1Row: 1,
-    bear2Row: 3
+    bear2Row: 2
   },
   {
     id: 2,
-    bear1: mockBears[2],
-    bear2: mockBears[3],
-    nextMatchup: 6,
-    pickedWinner: undefined,
-    column: 1,
-    bear1Row: 8,
-    bear2Row: 10
-  },
-  {
-    id: 3,
-    bear1: mockBears[4],
-    bear2: mockBears[5],
-    nextMatchup: 7,
-    pickedWinner: undefined,
-    column: 7,
-    bear1Row: 1,
-    bear2Row: 3
-  },
-  {
-    id: 4,
-    bear1: mockBears[6],
-    bear2: mockBears[7],
-    nextMatchup: 8,
-    pickedWinner: undefined,
-    column: 7,
-    bear1Row: 8,
-    bear2Row: 10
-  },
-  {
-    id: 5,
-    bear1: undefined,
+    bear1: mockBears[12],
     bear2: mockBears[8],
     nextMatchup: 9,
     pickedWinner: undefined,
-    column: 2,
-    bear1Row: 2,
+    column: 1,
+    bear1Row: 3,
     bear2Row: 4
+  },
+  {
+    id: 3,
+    bear1: mockBears[2],
+    bear2: mockBears[3],
+    nextMatchup: 10,
+    pickedWinner: undefined,
+    column: 1,
+    bear1Row: 5,
+    bear2Row: 6
+  },
+  {
+    id: 4,
+    bear1: mockBears[13],
+    bear2: mockBears[9],
+    nextMatchup: 10,
+    pickedWinner: undefined,
+    column: 1,
+    bear1Row: 7,
+    bear2Row: 8
+  },
+  // Round 1 - right side
+  {
+    id: 5,
+    bear1: mockBears[4],
+    bear2: mockBears[5],
+    nextMatchup: 11,
+    pickedWinner: undefined,
+    column: 7,
+    bear1Row: 1,
+    bear2Row: 2
   },
   {
     id: 6,
-    bear1: mockBears[9],
-    bear2: undefined,
-    nextMatchup: 9,
-    pickedWinner: undefined,
-    column: 2,
-    bear1Row: 7,
-    bear2Row: 9
-  },
-  {
-    id: 7,
-    bear1: undefined,
+    bear1: mockBears[14],
     bear2: mockBears[10],
-    nextMatchup: 10,
+    nextMatchup: 11,
     pickedWinner: undefined,
-    column: 6,
-    bear1Row: 2,
+    column: 7,
+    bear1Row: 3,
     bear2Row: 4
   },
   {
-    id: 8,
-    bear1: mockBears[11],
-    bear2: undefined,
-    nextMatchup: 10,
+    id: 7,
+    bear1: mockBears[6],
+    bear2: mockBears[7],
+    nextMatchup: 12,
     pickedWinner: undefined,
-    column: 6,
-    bear1Row: 7,
-    bear2Row: 9
+    column: 7,
+    bear1Row: 5,
+    bear2Row: 6
   },
+  {
+    id: 8,
+    bear1: mockBears[15],
+    bear2: mockBears[11],
+    nextMatchup: 12,
+    pickedWinner: undefined,
+    column: 7,
+    bear1Row: 7,
+    bear2Row: 8
+  },
+  // Quarterfinals
   {
     id: 9,
     bear1: undefined,
     bear2: undefined,
-    nextMatchup: 11,
+    nextMatchup: 13,
     pickedWinner: undefined,
-    column: 3,
-    bear1Row: 3,
-    bear2Row: 8
+    column: 2,
+    bear1Row: 2,
+    bear2Row: 4
   },
   {
     id: 10,
     bear1: undefined,
     bear2: undefined,
-    nextMatchup: 11,
+    nextMatchup: 13,
     pickedWinner: undefined,
-    column: 5,
-    bear1Row: 3,
-    bear2Row: 8
+    column: 2,
+    bear1Row: 5,
+    bear2Row: 7
   },
   {
     id: 11,
     bear1: undefined,
     bear2: undefined,
-    nextMatchup: 12,
+    nextMatchup: 14,
+    pickedWinner: undefined,
+    column: 6,
+    bear1Row: 2,
+    bear2Row: 4
+  },
+  {
+    id: 12,
+    bear1: undefined,
+    bear2: undefined,
+    nextMatchup: 14,
+    pickedWinner: undefined,
+    column: 6,
+    bear1Row: 5,
+    bear2Row: 7
+  },
+  // Semifinals
+  {
+    id: 13,
+    bear1: undefined,
+    bear2: undefined,
+    nextMatchup: 15,
+    pickedWinner: undefined,
+    column: 3,
+    bear1Row: 3,
+    bear2Row: 6
+  },
+  {
+    id: 14,
+    bear1: undefined,
+    bear2: undefined,
+    nextMatchup: 15,
+    pickedWinner: undefined,
+    column: 5,
+    bear1Row: 3,
+    bear2Row: 6
+  },
+  // Final
+  {
+    id: 15,
+    bear1: undefined,
+    bear2: undefined,
+    nextMatchup: 16,
     pickedWinner: undefined,
     column: 4,
-    bear1Row: 5,
-    bear2Row: 6
+    bear1Row: 4,
+    bear2Row: 5
   }
 ];
 
