83edfd819dd0e569703c561121eaad5c7ed86677
pwaronofsky <paularonofsky@gmail.com>
2026-09-21 10:15:04 -0400

    removed unused header

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useAuth0 } from '@auth0/auth0-react';
 import { useUserContext } from '../contexts/userContext';
 
 export const Header = React.forwardRef ((_, ref) => {
